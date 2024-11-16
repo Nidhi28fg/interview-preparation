eb25fc3dd3cdb06c8c63e372522a454660bae2aa
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-11-16 20:44:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ To include the File System module, use the require() method:
 Node.js फ़ाइल सिस्टम मॉड्यूल आपको अपने कंप्यूटर पर फ़ाइल सिस्टम के साथ काम करने की अनुमति देता है।
 फ़ाइल सिस्टम मॉड्यूल को शामिल करने के लिए, require()निम्न विधि का उपयोग करें:*/
 
-const data = fs.readFileSync('./a.txt', 'utf8');  /*file name*/
+const data = fs.readFileSync('./a.txt', 'utf8');  /*file name,  utf8 translates numbers into binary.*/ 
 
 
 console.log(data); /* in node.js you can run file locally */
